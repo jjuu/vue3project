ee33add877a2160adb62be938fb13dd29d3ce81a
Yeshua Kwok <yeshuame@gmail.com>
2025-02-12 22:30:03 +0900

    登录页面

--- README.md
+++ README.md
@@ -1,12 +1,44 @@
-# Vue 3 + TypeScript + Vite
+## 登录页面
 
-## 1. 路由: 学习插件vue-router的使用。
-- 一级路由：登录
-- 一级路由：home页面
-- 404
-- 任意路由
+学习了如下标签的使用。
 
-## 2. 
-- 在App.vue里用`<router-view></router-view>`引入路由导航。
-- 在`src/router`里配置的URL和页面的映射关系。这个和别的web项目的路由配置类似。
-- 在`src/views`下面写了所有的vue页面。这些页面是在`src/router`里映射过来的。
+```vue
+<el-row><el-col>
+<el-form><el-form-item><el-input>
+```
+
+从以下css代码中汲取了营养。
+
+```css
+<style scoped lang="scss">
+.login_container {
+    width: 100%;
+    height: 100vh;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+}
+
+.login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding: 40px;
+    h1 {
+        color: white;
+        font-size: 40px;
+    }
+    h2 {
+        font-size: 20px;
+        color: white;
+        margin: 20px 0px;
+    }
+}
+
+.login_btn {
+    width: 100%;
+}
+</style>
+```
+
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,68 @@
 <template>
-    <div>
-        <h1>一级路由：登录页面</h1>
+    <div class="login_container">
+        <el-row>
+            <!-- 「:xs="0"」：屏幕的宽度小于等于768的时候一份不占 -->
+            <el-col :span="12" :xs="0">占的位子</el-col>
+            <el-col :span="12" :xs="24">
+                <el-form class="login_form">
+                    <h1>Hello</h1>
+                    <h2>欢迎来到硅谷甄选</h2>
+                    <el-form-item>
+                        <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-button class="login_btn" type="primary" size="default">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
+        
+        <!-- <el-form></el-form> -->
     </div>
 </template>
 
 <script setup lang='ts'>
+import {User, Lock} from '@element-plus/icons-vue';
+import { reactive } from 'vue';
+
+// 收集帐号与密码的数据
+let loginForm = reactive({username: 'admin', password: '111111'});
+
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.login_container {
+    width: 100%;
+    height: 100vh;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    background-size: cover;
+}
 
+.login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url('@/assets/images/login_form.png') no-repeat;
+    background-size: cover;
+    padding: 40px;
+    h1 {
+        color: white;
+        font-size: 40px;
+    }
+    h2 {
+        font-size: 20px;
+        color: white;
+        margin: 20px 0px;
+    }
+}
+
+.login_btn {
+    width: 100%;
+}
 </style>
